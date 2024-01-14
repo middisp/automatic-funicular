011f2d9dde92ce806bf1b2b7671e6b3735387b88
Peter Middis <middisp@gmail.com>
2024-01-14 12:40:50 +0000

    content page

--- src/views/content.vue
+++ src/views/content.vue
@@ -1,8 +1,13 @@
 <template>
-  {{ contentId }}
+  <section class="content-grid content-page">
+    <h1>{{ title }}</h1>
+    <div v-html="content" />
+  </section>
 </template>
 
 <script>
+import getPageContent from '../constants/pageContent';
+
 import hero from '../components/hero.vue';
 
 export default {
@@ -10,6 +15,29 @@ export default {
   props: ['contentId'],
   components: {
     hero
+  },
+  data () {
+    return {
+      title: null,
+      content: null
+    }
+  },
+  created () {
+    const data = getPageContent(this.contentId);
+    this.title = data.title;
+    this.content = data.content;
   }
 }
 </script>
+
+<style lang="scss">
+.content-page {
+  gap: 3rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+}
+</style>
